5b916d12a14dfb866b6e9580f474c163bfd9e8cf
leandrocgsi <leandrocgsi@gmail.com>
2025-05-03 17:54:36 -0300

    Spring AI MCP - Account MCP Server

---
+++ 14_MCP/account-erudio-mcp-service/src/main/java/br/com/erudio/tools/config/MCPConfigs.java
@@ -0,0 +1,18 @@
+package br.com.erudio.tools.config;
+
+import br.com.erudio.tools.AccountTools;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MCPConfigs {
+
+    @Bean
+    public ToolCallbackProvider tools(AccountTools accountTools) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(accountTools)
+                .build();
+    }
+}
